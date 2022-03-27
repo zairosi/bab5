62425056674e5d4be5cf64a97d2166842b44847a
Rossi <ahmad.zairozi125@gmail.com>
2022-03-27 12:00:48 +0700

    Commit Massage

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
 import '../model/article.dart';
 
 class ApiService {
-  static final String _baseUrl = 'https://newsapi.org/v2';
+  static final String _baseUrl = 'https://newsapi.org/v2/';
   static final String _apiKey = '07873659c4b045b0bdd66093a63f2aa6';
   static final String _category = 'business';
   static final String _country = 'id';
